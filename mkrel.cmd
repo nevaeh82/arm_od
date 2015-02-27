ce2cb879c233f4ac1a4ac6e92a950c0a7888f467
Pavel Yarovoy <pyarovoy@stc-spb.ru>
2015-02-27 15:36:39 +0300

    mkrel and installator script

--- mkrel.cmd
+++ mkrel.cmd
@@ -1,20 +1,22 @@
 @echo off
 
-pushd 
-cd /d %~dp0
+set PATH=%PATH%;%~dp0redist\nsis
+set PATH=%PATH%;C:\Qt\4.8.6msvc;
+set PATH=%PATH%;C:\Python27
+set PATH=%PATH%;"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC";"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin";
+set QTDIR=%QTDIR%;C:\Qt\4.8.6msvc;
+set INCLUDE=%INCLUDE%;"c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\INCLUDE;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\ATLMFC\INCLUDE;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\include"
+set LIBPATH=%LIBPATH%;"C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\Windows\Microsoft.NET\Framework\v3.5;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\LIB;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\ATLMFC\LIB"
+set LIB=%LIB%;"c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\LIB;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\ATLMFC\LIB;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\lib"
 
-if "%PRODUCT_PLATFORM%"=="" (
-	echo Error: Platform not defined!
-	exit /B 1
-)
+cmake -E remove_directory build
+cmake -E make_directory  build
 
-call %PRODUCT_PLATFORM%\activate.cmd msvc
+cd build
 
-set MAKE_COMMAND=mingw32-make -r
+cmake -E remove CMakeCache.txt
 
-qtx make clean msvc-release mkrel
+cmake .. -G "NMake Makefiles"
 
-if errorlevel 1 exit /B 1 
-
-popd
-exit /B 0 
+nmake
+nmake install package
