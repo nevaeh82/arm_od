df7171ddcc9e891629c18d07abded8bf918951ee
Kirill Sudarushkin <ksudarushkin@stc-spb.ru>
2015-03-13 16:18:48 +0300

    Added ordering for update SQL files in create scripts

--- sql/create_db.cmd
+++ sql/create_db.cmd
@@ -4,6 +4,6 @@ rem create origin database
 mysql.exe -u%1 -p%2 --default-character-set=utf8 < %~dp0\arm_od_uav_tables.sql
 
 rem apply updates
-for /f %%f in ('dir /b %~dp0\updates') do (
+for /f %%f in ('dir /b /on %~dp0\updates') do (
 	mysql.exe -u%1 -p%2 --default-character-set=utf8 --database=uav < %~dp0\updates\%%f
 )
